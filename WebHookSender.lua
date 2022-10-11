bdb579e3150555503957500ba95820ea2e168a12
Bomber874 <Bomber033@yandex.ru>
2022-10-11 20:35:00 +0700

    Забыл изменить

--- WebHookSender.lua
+++ WebHookSender.lua
@@ -3,7 +3,7 @@
 function on_event(event)
 	if event == obs.OBS_FRONTEND_EVENT_STREAMING_STARTED then
 		os.execute("start WebhookSenderForOBS.exe start")
-	elseif event == obs.OBS_FRONTEND_EVENT_STREAMING_STARTED then
+	elseif event == obs.OBS_FRONTEND_EVENT_STREAMING_STOPPED then
 		os.execute("start WebhookSenderForOBS.exe stop")
 	end
 end
